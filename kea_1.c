0cb5ac47f2531a6c8effd4e19ba4ad25a2a23802
Abel Thampi <abelthampi3@gmail.com>
2026-08-31 20:51:15 +0530

    Add files via upload

---
+++ kea_1.c
@@ -0,0 +1,84 @@
+/* C program to implement the following operations using functions:
+
+1. Read two matrices from the user.
+2. Perform matrix addition.
+3. Display the resultant matrix.
+4. Use separate functions for reading, addition, and displaying the matrices. */
+
+#include<stdio.h>
+#include<conio.h>
+int print(int a[10][10],int b[10][10],int r,int c);
+int read(int a[10][10],int b[10][10],int r,int c);
+int sum(int a[10][10],int b[10][10],int r,int c);
+void main()
+{
+	int a[10][10],b[10][10],i,j,c,r,add;
+	printf("Enter the number of rows and columns : ");
+	scanf("%d%d",&r,&c);
+	read(a,b,r,c);
+	print(a,b,r,c);
+	sum(a,b,r,c);
+		
+}
+int read(int a[10][10],int b[10][10],int r,int c)
+{
+	int i,j;
+
+	printf("Enter the elements of First Matrix : \n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter the elements of Second Matrix : \n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+}
+
+int print(int a[10][10],int b[10][10],int r,int c)
+{
+	int i,j;
+	printf("****** First Matrix ******\n");
+	printf("\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("****** Second Matrix ******\n");
+		printf("\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int sum(int a[10][10],int b[10][10],int c,int r)
+{
+	int s[10][10],j,i;
+	printf("****** Sum is ******\n");
+	printf("\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			s[i][j]=a[i][j]+b[i][j];
+			printf("%d\t",s[i][j]);
+		}
+		printf("\n");
+	}
+}
